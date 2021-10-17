03585f73d42fdf71b44a8b751d585a8a6f824ff9
Kevin Kuang <200sxmeda@gmail.com>
2021-10-17 16:56:52 +1100

    Fix incorrect require in run.js, add deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,30 +1,29 @@
-const main = async() => {
+const main = async () => {
     const nftContractFactory = await hre.ethers.getContractFactory('MyEpicNFT');
-    const nftContract = await nftContractFactory.deploy(); 
+    const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
-    console.log("Contract deployed to:", nftContract.address); 
-
+    console.log("Contract deployed to:", nftContract.address);
+  
     // Call the function.
     let txn = await nftContract.makeAnEpicNFT()
     // Wait for it to be mined.
     await txn.wait()
-
-    // Mint another NFT for fun.
+    console.log("Minted NFT #1")
+  
     txn = await nftContract.makeAnEpicNFT()
     // Wait for it to be mined.
     await txn.wait()
-
-};
-
-const runMain = async() => {
+    console.log("Minted NFT #2")
+  };
+  
+  const runMain = async () => {
     try {
-        await main();
-        process.exit(0);
+      await main();
+      process.exit(0);
     } catch (error) {
-        console.log(error);
-        process.exit(1);
+      console.log(error);
+      process.exit(1);
     }
-};
-
-runMain(); 
-
+  };
+  
+  runMain();
