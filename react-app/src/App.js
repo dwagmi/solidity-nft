14a1cb2b5e1435e229133c9bb1b05bda5e955fc7
Kevin Kuang <200sxmeda@gmail.com>
2021-10-17 23:52:10 +1100

    Add check for whether connected wallet is on rinkeby network

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -29,6 +29,14 @@ const App = () => {
       const accounts = await ethereum.request({ method: 'eth_accounts' });
 
       if (accounts.length !== 0) {
+        let chainId = await ethereum.request({ method: 'eth_chainId' });
+        console.log("Connected to chain " + chainId);
+
+        // String, hex code of the chainId of the Rinkebey test network
+        const rinkebyChainId = "0x4"; 
+        if (chainId !== rinkebyChainId) {
+          alert("You are not connected to the Rinkeby Test Network!");
+        }
           const account = accounts[0];
           console.log("Found an authorized account:", account);
 					setCurrentAccount(account)
